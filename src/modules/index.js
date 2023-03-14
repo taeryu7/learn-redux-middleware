5b5020bc0dd5c13a6618d4c6dc6d129229ab6321
taeryu7 <pegasy02@gmail.com>
2023-03-14 11:18:41 +0900

    0314_before

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,4 +1,18 @@
 
+// redux-saga 로 프로미스 다루기
+import { combineReducers } from 'redux';
+import counter, { counterSaga } from './counter';
+import posts, { postsSaga } from './posts';
+import { all } from 'redux-saga/effects';
+
+const rootReducer = combineReducers({ counter, posts });
+export function* rootSaga() {
+  yield all([counterSaga(), postsSaga()]); // all 은 배열 안의 여러 사가를 동시에 실행시켜준다.
+}
+
+export default rootReducer;
+
+/*
 // 리덕스 사가 설치 및 비동기 카운터 만들기
 import { combineReducers } from 'redux';
 import counter, { counterSaga } from './counter';
@@ -11,6 +25,7 @@ export function* rootSaga() {
 }
 
 export default rootReducer;
+*/
 
 /*
 import { combineReducers } from 'redux';
--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,99 @@
 
+// redux-saga 로 프로미스 다루기
+
+import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
+import {
+  reducerUtils,
+  handleAsyncActions,
+  handleAsyncActionsById
+} from '../lib/asyncUtils';
+import { call, put, takeEvery } from 'redux-saga/effects';
+
+// 액션 타입
+
+// 포스트 여러개 조회하기
+const GET_POSTS = 'GET_POSTS'; // 요청 시작
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
+
+// 포스트 하나 조회하기
+const GET_POST = 'GET_POST';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_ERROR = 'GET_POST_ERROR';
+
+export const getPosts = () => ({ type: GET_POSTS });
+// payload는 파라미터 용도, meta는 리듀서에서 id를 알기위한 용도
+export const getPost = id => ({ type: GET_POST, payload: id, meta: id });
+
+function* getPostsSaga() {
+  try {
+    const posts = yield call(postsAPI.getPosts); // call 을 사용하면 특정 함수를 호출하고, 결과물이 반환 될 때까지 기다려줄 수 있다.
+    yield put({
+      type: GET_POSTS_SUCCESS,
+      payload: posts
+    }); // 성공 액션 디스패치
+  } catch (e) {
+    yield put({
+      type: GET_POSTS_ERROR,
+      error: true,
+      payload: e
+    }); // 실패 액션 디스패치
+  }
+}
+
+// 액션이 지니고 있는 값을 조회하고 싶다면 action을 파라미터로 받아와서 사용 할 수 있다.
+function* getPostSaga(action) {
+  const param = action.payload;
+  const id = action.meta;
+  try {
+    const post = yield call(postsAPI.getPostById, param); // API 함수에 넣어주고 싶은 인자는 call 함수의 두번째 인자부터 순서대로 넣어주면 된다.
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post,
+      meta: id
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_ERROR,
+      error: true,
+      payload: e,
+      meta: id
+    });
+  }
+}
+
+// 사가들을 합치기
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
+
+// 3번째 인자를 사용하면 withExtraArgument 에서 넣어준 값들을 사용 할 수 있다.
+export const goToHome = () => (dispatch, getState, { history }) => {
+  history.push('/');
+};
+
+// initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했다.
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+    case GET_POSTS_SUCCESS:
+    case GET_POSTS_ERROR:
+      return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
+    case GET_POST:
+    case GET_POST_SUCCESS:
+    case GET_POST_ERROR:
+      return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+    default:
+      return state;
+  }
+}
+
 /*
 // 포스트 조회시 재로딩 문제 해결하기
 특정 포스트를 조회하는 과정에서 재로딩 문제를 해결하려면, 방급했던 방식으로 처리 할 수 없다.
@@ -8,7 +103,7 @@
 1. 컴포넌트가 언마운트 될 때 포스트 내용을 비우도록 하는것이다.
 
 이 작업을 하려면 posts 리덕스 모듈에 CLEAR_POST 라는 액션을 준비해야한다.
-*/
+
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
 import {
   createPromiseThunk,
@@ -60,8 +155,7 @@ export const goToHome = () => (dispatch, getState, { history }) => {
   history.push('/');
 };
 // PostContainer 컴포넌트의 useEffect 의 cleanup 함수 (useEffect 에서 반환하는 함수)에서 해당 액션을 디스패치해주면 된다.
-
-
+*/
 
 /*
 // redux-thunk로 프로미스 다루기
