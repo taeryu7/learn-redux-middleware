0e67b2bb40c8a14a433c4bfb3e525f4d8bdfd4b1
taeryu7 <pegasy02@gmail.com>
2023-03-14 13:50:40 +0900

    0314_after_1

--- src/modules/posts.js
+++ src/modules/posts.js
@@ -1,4 +1,68 @@
 
+// 프로미스를 처리하는 사가 리팩토링
+import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
+import {
+  reducerUtils,
+  handleAsyncActions,
+  handleAsyncActionsById,
+  createPromiseSaga,
+  createPromiseSagaById
+} from '../lib/asyncUtils';
+import { takeEvery } from 'redux-saga/effects';
+
+// 액션 타입
+
+// 포스트 여러개 조회하기
+const GET_POSTS = 'GET_POSTS'; // 요청 시작
+const GET_POSTS_SUCCESS = 'GET_POSTS_SUCCESS'; // 요청 성공
+const GET_POSTS_ERROR = 'GET_POSTS_ERROR'; // 요청 실패
+
+// 포스트 하나 조회하기
+const GET_POST = 'GET_POST';
+const GET_POST_SUCCESS = 'GET_POST_SUCCESS';
+const GET_POST_ERROR = 'GET_POST_ERROR';
+
+export const getPosts = () => ({ type: GET_POSTS });
+export const getPost = id => ({ type: GET_POST, payload: id, meta: id });
+
+const getPostsSaga = createPromiseSaga(GET_POSTS, postsAPI.getPosts);
+const getPostSaga = createPromiseSagaById(GET_POST, postsAPI.getPostById);
+
+// 사가들을 합치기
+export function* postsSaga() {
+  yield takeEvery(GET_POSTS, getPostsSaga);
+  yield takeEvery(GET_POST, getPostSaga);
+}
+
+// 3번째 인자를 사용하면 withExtraArgument 에서 넣어준 값들을 사용 할 수 있다.
+export const goToHome = () => (dispatch, getState, { history }) => {
+  history.push('/');
+};
+
+// initialState 쪽도 반복되는 코드를 initial() 함수를 사용해서 리팩토링 했다.
+const initialState = {
+  posts: reducerUtils.initial(),
+  post: reducerUtils.initial()
+};
+
+export default function posts(state = initialState, action) {
+  switch (action.type) {
+    case GET_POSTS:
+    case GET_POSTS_SUCCESS:
+    case GET_POSTS_ERROR:
+      return handleAsyncActions(GET_POSTS, 'posts', true)(state, action);
+    case GET_POST:
+    case GET_POST_SUCCESS:
+    case GET_POST_ERROR:
+      return handleAsyncActionsById(GET_POST, 'post', true)(state, action);
+    default:
+      return state;
+  }
+}
+
+
+
+/*
 // redux-saga 로 프로미스 다루기
 
 import * as postsAPI from '../api/posts'; // api/posts 안의 함수 모두 불러오기
@@ -93,6 +157,7 @@ export default function posts(state = initialState, action) {
       return state;
   }
 }
+*/
 
 /*
 // 포스트 조회시 재로딩 문제 해결하기
